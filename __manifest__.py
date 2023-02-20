638c3b0f6b220499c22de729af5db55c72d72678
KristinBrGultom <kristingultom190819@gmail.com>
2023-02-20 10:35:32 +0700

    Praktikum 1 PSD

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name"          : "New Addons",
+    "Version"       : "1.0",
+    " author"       : "Kristin Br Gultom",
+    "website"       : "https://github.com/KristinBrGultom/",
+    "category"      : "New Module",
+    "summary"       : "Ini adalah addons baru",
+    "description"   : """
+        Latihan membuat addons baru di odoo 1
+    """,
+    "depends"       : [
+        "base",
+    ],
+    "data"          : [
+    
+    ],
+    "demo"          : [],
+    "test"          : [],
+    "image"         : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+}
